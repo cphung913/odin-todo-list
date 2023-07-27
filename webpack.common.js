2449cbf26727b91d08f7b741632ab6a5af5ca313
cphung913 <chasephung13@gmail.com>
2023-07-26 23:10:28 -0700

    setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
+            }
+        ]
+    },
+}
+
+//npm init -y 
+//npm instal --save-dev webpack webpack-cli style-loader css-loader html-webpack-plugin webpack-merge webpack-dev-server html-loader file-loader clean-webpack-plugin mini-css-extract-plugin  css-minimizer-webpack-plugin
+
+
+//installs
+
+//webpack
+//webpack-cli
+//style-loader
+//css-loader
+//html-webpack-plugin
+//webpack-merge
+//webpack-dev-server
+//html-loader
+//file-loader
+//clean-webpack-plugin
+//mini-css-extract-plugin
+//css-minimizer-webpack-plugin
---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        filename: "main.[contenthash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
+        new HtmlWebpackPlugin({ 
+            template: "./src/template.html",
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+        ]
+    },
+    optimization: {
+        minimizer: [
+          // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+          // `...`,
+          new CssMinimizerPlugin(),
+          new TerserPlugin(),
+        ],
+      },
+})
