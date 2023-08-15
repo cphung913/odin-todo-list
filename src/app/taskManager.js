2e5833f008c61bbdad80b994c6db3ef99ea6937c
cphung913 <chasephung13@gmail.com>
2023-08-14 22:44:37 -0700

    added local storage

--- src/app/taskManager.js
+++ src/app/taskManager.js
@@ -1,15 +1,18 @@
-let home = [];
+let home = localStorage.getItem("home") ? JSON.parse(localStorage.getItem("home")) : [];
 let today = [];
-let all = [];
-let projects = {};
+let all = localStorage.getItem("all") ? JSON.parse(localStorage.getItem("all")) : [];
+let projects = localStorage.getItem("projects") ? JSON.parse(localStorage.getItem("projects")) : {};
 
 function addTask(task, path) {
     if (path) {
         projects[path].push(task);
+        localStorage.setItem("projects", JSON.stringify(projects));
     } else {
         home.push(task);
+        localStorage.setItem("home", JSON.stringify(home));
     }
     all.push(task);
+    localStorage.setItem("all", JSON.stringify(all));
 }
 
 function setupToday() {
@@ -39,6 +42,7 @@ function getTasks(path, isProject = false) {
 function addProject(name) {
     if (projects[name]) return true;
     projects[name] = [];
+    localStorage.setItem("projects", JSON.stringify(projects));
 }
 
 function getProjects() {
